92f77d01faaea3c0e4bb2133bc1ab3b31f5a7b9b
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-12-15 19:24:27 +0300

    feat: set the structure of the project

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const baseConfig = {
 	entry: path.resolve(__dirname, './src/index.ts'),
@@ -15,13 +16,24 @@ const baseConfig = {
 				exclude: /node_modules/,
 			},
 			{
+				test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+				type: 'asset/resource',
+			},
+			{
 				test: /\.s[ac]ss$/i,
 				use: [
 					"style-loader",
 					"css-loader",
-					"sass-loader",
+					"sass-loader"
 				],
 			},
+			{
+				test: /\.(woff|woff2|ttf)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name].[ext]'
+				}
+			}
 		],
 	},
 	resolve: {
@@ -30,6 +42,7 @@ const baseConfig = {
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[name][ext]',
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
@@ -37,6 +50,9 @@ const baseConfig = {
 			filename: 'index.html',
 		}),
 		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		})
 	],
 };
 

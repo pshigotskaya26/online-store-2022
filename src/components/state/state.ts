910f832cab788ceea7ae3ed566eea3b7720ecb62
sklevzhic <sklevzhic@gmail.com>
2022-12-17 23:19:37 +0300

    feat: added dafault state

---
+++ src/components/state/state.ts
@@ -0,0 +1,13 @@
+const defaultState: any = {
+    products: [],
+    filterParams: {
+        brand: [],
+        color: [],
+        year: [],
+        searchQuery: ""
+    },
+    filteredProducts: [],
+    cart: []
+}
+
+export default defaultState
